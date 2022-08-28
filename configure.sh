71cca145ea3c25ac0a0b85f35cf8bdca6b1976c0
Caleb Palmer <palmer.caleb@gmail.com>
2022-08-28 19:23:25 -0300

    Updated configure.sh

--- configure.sh
+++ configure.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+set -e
+set -x
 
 BUILDDIR=$1
 INSTALLDIR=$BUILDDIR/install
 BUILDTYPE=$2
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
+mkdir -p $BUILDDIR
+pushd $BUILDDIR
 
-cmake -S. -B$BUILDDIR \
+# export CC=/usr/bin/gcc
+# export CXX=/usr/bin/g++
+
+conan install $SCRIPT_DIR --profile $SCRIPT_DIR/conanprofile.txt --build missing
+
+cmake -S${SCRIPT_DIR} -B. \
       -GNinja \
       -DCMAKE_INSTALL_PREFIX=$INSTALLDIR \
       -DCMAKE_BUILD_TYPE=$BUILDTYPE \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+
+popd
